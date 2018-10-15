7aa0e1f1bf1c15252e1d7f6d0d3ce5c26f4700ac
eslam-propertzcrm <eslam.propertzcrm@gmail.com>
2018-10-15 15:43:10 +0200

    Eslam

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,27 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
+   	// return App\User::all();
     return $request->user();
 });
+
+Route::get('login', ['as'=>'login', 'uses'=>function () {
+	return response(['status'=>401, 'messages'=>'sorry you are not AUTHENTICATED'], 401);
+}]);
+
+Route::post('login', 'Api\Auth\LoginController@index');
+
+Route::post('register', 'Api\Auth\RegisterController@index');
+
+Route::get('test', 'Api\Auth\RegisterController@test');
+
+/*,'namespace'=>'Api'  you can use namespace rather than Api\NameController */
+
+
+
+Route::group(['middleware'=>'auth:api'], function(){
+	Route::resource('sections', 'Api\SectionsController', ['except' => ['create', 'edit']]);
+	Route::resource('notes', 'Api\NotesController', ['except' => ['create', 'edit']]);
+	Route::get('sections/{id}/notes', 'Api\NotesController@show');
+});
+	// Route::delete('sections/{id}/notes', 'Api\NotesController@destroy');
